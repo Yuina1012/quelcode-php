acd6aee3095d8ce3f7938dabf646a343d6678a9d
小泉結以奈 <mizugoro1012@gmail.com>
2020-08-18 18:41:34 +0900

    $nを＄jに変更

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -3,14 +3,14 @@ $array = explode(',', $_GET['array']);
 
 // 修正はここから
 for ($i  = 0 ; $i < count ($array) ; $i ++ ) {
-      for ($n =1; $n <count ($array) ; $n ++ ){
-        if ($array [$n - 1]   >  $array [ $n ]){
+      for ($j =1; $j <count ($array) ; $j ++ ){
+        if ($array [$j - 1]   >  $array [ $j ]){
           //退避
-          $tmp = $array [ $n ];
+          $tmp = $array [ $j ];
           //入れる
-          $array [ $n ] = $array [ $n -1 ];
+          $array [ $j ] = $array [ $j -1 ];
           //退避してたのを入れる
-          $array [ $n -1 ] = $tmp;
+          $array [ $j -1 ] = $tmp;
         }
       }  
 }
