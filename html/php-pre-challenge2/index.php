7a439f7994cc8a5ac2750490997a64e2468ad92f
小泉結以奈 <mizugoro1012@gmail.com>
2020-08-18 18:38:47 +0900

    退避に修正

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -2,15 +2,15 @@
 $array = explode(',', $_GET['array']);
 
 // 修正はここから
-for ($i = 0; $i < count($array); $i++) {
-      for ($n=1; $n<count($array);$n++){
-        if($array[$n-1] > $array[$n]){
-          //待避
-          $tmp=$array[$n];
+for ($i  = 0 ; $i < count ($array) ; $i ++ ) {
+      for ($n =1; $n <count ($array) ; $n ++ ){
+        if ($array [$n - 1]   >  $array [ $n ]){
+          //退避
+          $tmp = $array [ $n ];
           //入れる
-          $array[$n]=$array[$n-1];
-          //待避してたのを入れる
-          $array[$n-1]=$tmp;
+          $array [ $n ] = $array [ $n -1 ];
+          //退避してたのを入れる
+          $array [ $n -1 ] = $tmp;
         }
       }  
 }
