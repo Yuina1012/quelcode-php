6b2493d3e3ea59e6a5f76a9841d11af06daa398e
小泉結以奈 <mizugoro1012@gmail.com>
2020-08-20 13:06:30 +0900

    インデントの調整

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -3,16 +3,16 @@ $array = explode(',', $_GET['array']);
 
 // 修正はここから
 for ($i  = 0 ; $i < count($array) ; $i++)  {
-      for ($j = 1 ; $j < count($array) ; $j++) {
+    for ($j = 1 ; $j < count($array) ; $j++) {
         if ($array [$j - 1]   >  $array [ $j ]){
-          //退避
-          $tmp = $array[ $j ];
-          //入れる
-          $array[ $j ] = $array[ $j -1 ];
-          //退避してたのを入れる
-          $array[$j - 1] = $tmp;
+            //退避
+            $tmp = $array[ $j ];
+            //入れる
+            $array[ $j ] = $array[ $j -1 ];
+            //退避してたのを入れる
+            $array[$j - 1] = $tmp;
         }
-      }  
+    }  
 }
 // 修正はここまで
 
