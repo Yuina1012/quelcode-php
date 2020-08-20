ae1d353e0d30b9fa9ee4cf4d85f99d1caadac1b2
小泉結以奈 <mizugoro1012@gmail.com>
2020-08-20 13:04:43 +0900

    スペースの調整

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -2,15 +2,15 @@
 $array = explode(',', $_GET['array']);
 
 // 修正はここから
-for ($i  = 0 ; $i < count ($array) ; $i ++ ) {
-      for ($j =1; $j <count ($array) ; $j ++ ){
+for ($i  = 0 ; $i < count($array) ; $i++)  {
+      for ($j = 1 ; $j < count($array) ; $j++) {
         if ($array [$j - 1]   >  $array [ $j ]){
           //退避
-          $tmp = $array [ $j ];
+          $tmp = $array[ $j ];
           //入れる
-          $array [ $j ] = $array [ $j -1 ];
+          $array[ $j ] = $array[ $j -1 ];
           //退避してたのを入れる
-          $array [ $j -1 ] = $tmp;
+          $array[$j - 1] = $tmp;
         }
       }  
 }
